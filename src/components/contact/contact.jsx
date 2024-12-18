8034402a16e14026480834520dad951acecb40e1
SardorQobilov <sardorqobilov555@gmail.com>
2024-12-18 12:25:02 +0500

    S

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -199,8 +199,69 @@ export const Contact = () => {
     <>
       <section className='contact' id='contact'>
         <div className='container'>
+          {' '}
           <div className='contact-top'>
-            <Title subText='контакты' />
+            <Title subText='контакты' />{' '}
+            <div className='contact-top__boxes'>
+              {' '}
+              <div className='content-box'>
+                {' '}
+                <div className='box'>
+                  <img src={assets.bigLocation} alt='' />
+                  <h3>Адрес</h3>{' '}
+                </div>
+                <a href='#!'>г. Лучший, ул. Милая 6</a>{' '}
+              </div>{' '}
+              <div className='midle-boxes'>
+                {' '}
+                <div className='flex-box'>
+                  {' '}
+                  <div className='content-box'>
+                    {' '}
+                    <div className='box'>
+                      <img src={assets.tel} alt='' width={31} height={31} />
+                      <h3>Телефон</h3>{' '}
+                    </div>
+                    <a href='#!'>8 888 888 88 88</a>
+                    <a href='#!'>8 999 999 99 99</a>{' '}
+                  </div>{' '}
+                  <div className='content-box'>
+                    {' '}
+                    <div className='box'>
+                      {' '}
+                      <img src={assets.clock} alt='' width={31} height={31} />
+                      <h3>Время работы</h3>{' '}
+                    </div>{' '}
+                    <a href='#!'>
+                      Понедельник – Воскресенье <br />с 8:00 до 20:00{' '}
+                    </a>{' '}
+                  </div>{' '}
+                </div>{' '}
+                <div className='social-media'>
+                  {' '}
+                  <a href='#!'>
+                    <img src={assets.telegram} alt='' />{' '}
+                  </a>{' '}
+                  <a href='#!'>
+                    <img src={assets.whatsapp} alt='' />{' '}
+                  </a>{' '}
+                </div>{' '}
+              </div>{' '}
+              <div className='content-box'>
+                {' '}
+                <div className='box'>
+                  <img src={assets.pochta} alt='' width={36} height={28} />
+                  <h3>Почта</h3>{' '}
+                </div>
+                <a href='#!'>pochta@mail.com</a>{' '}
+              </div>{' '}
+            </div>{' '}
+            <a className='yellow-btn' href='#!'>
+              Заказать звонок{' '}
+            </a>{' '}
+          </div>
+          <div className=''>
+            <Title text='Остались вопросы?' />
             <div className='contact-bottom'>
               <form ref={contactFormRef}>
                 <div className='inputs'>
